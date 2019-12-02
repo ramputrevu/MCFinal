2e673a54d123aca419cfba1af46d084e95fb9713
ram <ramputrevu10@gmail.com>
2019-12-02 16:24:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mcfinal/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mcfinal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+//import com.example.mcfinal.Main2Activity;
+//import com.example.mcfinal.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button next = (Button) findViewById(R.id.signin);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Main2Activity.class);
+                startActivityForResult(myIntent, 0);
+
+            }
+
+        });
+    }
+}
