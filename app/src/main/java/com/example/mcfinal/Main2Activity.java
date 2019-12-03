1171587b6de5b708cb13278f42c493135f4f8e9f
akhashogji <52230109+akhashogji@users.noreply.github.com>
2019-12-03 02:39:16 -0500

    Implemented Db and made the login authenticate + pull categories from the Db

--- app/src/main/java/com/example/mcfinal/Main2Activity.java
+++ app/src/main/java/com/example/mcfinal/Main2Activity.java
@@ -1,27 +1,82 @@
 package com.example.mcfinal;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.mcfinal.Main3Activity;
 import com.example.mcfinal.R;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main2Activity extends Activity {
+    DatabaseHelper databaseHelper;
+    Spinner spinner;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Button next = (Button) findViewById(R.id.laptops);
+        databaseHelper = new DatabaseHelper(Main2Activity.this);
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        // get a cursor from the Db
+        Cursor c = databaseHelper.openDatabase().rawQuery("SELECT DISTINCT product_type FROM products", null);
+        ArrayList<String> spinnerContent = new ArrayList<String>();
+        if(c.moveToFirst()) {
+            do{
+                String word = c.getString(c.getColumnIndexOrThrow("product_type"));
+                spinnerContent.add(word);
+            }while(c.moveToNext());
+        }
+        c.close();
+
+        String[] allSpinner = new String[spinnerContent.size()];
+        allSpinner = spinnerContent.toArray(allSpinner);
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(Main2Activity.this,android.R.layout.simple_spinner_item, allSpinner);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                return;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        Button next = (Button) findViewById(R.id.submit_category);
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), Main3Activity.class);
-                startActivityForResult(myIntent, 0);
-
+                Bundle extras = getIntent().getExtras();
+                String username, product_type = null;
+                spinner = (Spinner) findViewById(R.id.spinner);
+                spinner.getSelectedItem().toString();
+                if(extras != null) {
+                    username = extras.getString("username");
+                    Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+                    intent.putExtra("username", username);
+                    intent.putExtra("product_type", product_type);
+                    startActivity(intent);
+                }
             }
 
         });
--- app/src/main/java/com/example/mcfinal/MainActivity.java
+++ app/src/main/java/com/example/mcfinal/MainActivity.java
@@ -5,24 +5,47 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 //import com.example.mcfinal.Main2Activity;
 //import com.example.mcfinal.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    EditText edtEmail;
+    EditText edtPassword;
+    DatabaseHelper databaseHelper;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button next = (Button) findViewById(R.id.signin);
-        next.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), Main2Activity.class);
-                startActivityForResult(myIntent, 0);
+        edtEmail = (EditText) findViewById(R.id.EmailId);
+        edtPassword = (EditText) findViewById(R.id.loginpassword);
+        btnLogin = (Button) findViewById(R.id.signin);
 
-            }
+        databaseHelper = new DatabaseHelper(MainActivity.this);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    boolean isExist = databaseHelper.checkUserExist(edtEmail.getText().toString(), edtPassword.getText().toString());
+
+                    if(isExist){
+                        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                        intent.putExtra("username", edtEmail.getText().toString());
+                        startActivity(intent);
+                    } else {
+                        edtPassword.setText(null);
+                        Toast.makeText(MainActivity.this, "Login failed. Invalid username or password.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+//                Intent myIntent = new Intent(view.getContext(), Main2Activity.class);
+//                startActivityForResult(myIntent, 0);
 
         });
     }
